54411b6591a1460f421cfd9076b22c90ef58d425
Dominic Christensen-Abeyta <christensendominic@yahoo.com>
2024-02-09 15:53:11 -0700

    Unit1c
    Lab and challenge

---
+++ Unit1c/Unit1cLab.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Lab
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number of rows in the pyramid: ");
+        int rows = int.Parse(Console.ReadLine());
+
+        if (rows <= 0)
+        {
+            //ensure the number is not a negative
+            Console.WriteLine("Please enter a positive number.");
+            return;
+        }
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                //use Console.Write() to make sure all index stay on the same line
+                Console.Write(i);
+            }
+            //use Console.WriteLine() to create new line
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+}
