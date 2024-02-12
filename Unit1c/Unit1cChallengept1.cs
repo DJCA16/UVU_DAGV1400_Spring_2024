aaaac8640c150b2b4b1788f423f5ad5b6ef32962
Dominic Christensen-Abeyta <christensendominic@yahoo.com>
2024-02-12 12:43:02 -0700

    Unit 1 Challenge
    2 parts

---
+++ Unit1c/Unit1cChallengept1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Unit1cChallengept1
+{
+    public static void Main(string[] args)
+    {
+        Random number = new Random();
+        int randNum = number.Next(1, 11);
+        bool success = false;
+        while (success == false)
+        {
+            Console.WriteLine("Guess a number 1 though 10: ");
+            int guessNum = Convert.ToInt32(Console.ReadLine());
+            success = userGuess(randNum, guessNum);
+        }
+    }
+    public static bool userGuess(int randNum, int guessNum)
+    {
+        bool guessedCorrectly = false;
+        if (guessNum <= 10 && guessNum > 0)
+        {
+            if (guessNum > randNum)
+            {
+                Console.WriteLine("Your guess is too high. Guess again: ");
+            }
+            else if (guessNum < randNum)
+            {
+                Console.WriteLine("Your guess is too low. Guess again: ");
+            }
+            else if (guessNum == randNum)
+            {
+                guessedCorrectly = true;
+                Console.WriteLine("Congrats!! You guessed correctly!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please enter a number between 1 and 10");
+        }
+        return guessedCorrectly;
+    }
+    
+}
---
+++ Unit1c/Unit1cChallengept2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class partTwo
+{
+    public static void Main(string[] args)
+    {
+        string[] foods = new string[3];
+
+        Console.WriteLine("Enter your three favorite foods: ");
+        try
+        {
+            if (foods.Length == 3)
+            {
+                for (int i = 0; i < foods.Length; i++)
+                {
+                    foods[i] = Console.ReadLine();
+                }
+
+                for (int i = 0; i < foods.Length; i++)
+                {
+                    Console.WriteLine("I love " + foods[i] + "!");
+                }
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Please enter just three foods.");
+        }
+
+        Console.ReadKey();
+    }
+}
