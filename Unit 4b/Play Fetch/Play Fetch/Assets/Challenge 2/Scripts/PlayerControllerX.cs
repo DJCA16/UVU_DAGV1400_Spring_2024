f1fd487e5a05c6ac56a8cc03023f12e14ce1c0e6
Dominic Christensen-Abeyta <christensendominic@yahoo.com>
2024-04-08 12:55:07 -0600

    Play Fetch project
    play fetch

---
+++ Unit 4b/Play Fetch/Play Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float fireRate = 1; 
+    private float nextFire = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog after certain amount of time has passed
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate; //reset fireRate as soon as dog is fired
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
